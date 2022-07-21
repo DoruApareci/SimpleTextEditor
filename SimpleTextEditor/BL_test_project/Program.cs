9010cc97471bed4a44c0aa250e43ee7053d20103
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2022-07-21 21:23:25 +0300

    Second test

--- SimpleTextEditor/BL_test_project/Program.cs
+++ SimpleTextEditor/BL_test_project/Program.cs
@@ -2,14 +2,42 @@
 
 Editor _editor = new Editor();
 History _history = new History();
+EditorState _state;
 
-_editor.Content = "Hello World!";
+_editor.Content = "Hello World! 1";
 _history.Save(_editor);
+Console.WriteLine("load data 1");
+ShowData();
 _editor.Content = "Hello World! 2";
 _history.Save(_editor);
+Console.WriteLine("load data 2");
+ShowData();
+Console.WriteLine("data changed");
 _editor.Content = "Hello World! 3";
-_history.Save(_editor);
-Console.WriteLine("Content: " + _editor.Content);
-Console.WriteLine("History: " + _history.CanUndo);
-Console.WriteLine("History: " + _history.CanRedo);
-//_editor.Load(_history.UndoAction());
+
+
+ShowData();
+_editor.Load(_history.UndoAction());
+ShowData();
+_editor.Load(_history.UndoAction());
+ShowData();
+_editor.Load(_history.UndoAction());
+ShowData();
+_editor.Load(_history.RedoAction());
+ShowData();
+_editor.Load(_history.RedoAction());
+ShowData();
+_editor.Load(_history.RedoAction());
+ShowData();
+_editor.Load(_history.RedoAction());
+ShowData();
+
+
+
+void ShowData()
+{
+    Console.WriteLine("===============================");
+    Console.WriteLine("Content: " + _editor.Content);
+    Console.WriteLine("CanUndo: " + _history.CanUndo);
+    Console.WriteLine("CanRedo: " + _history.CanRedo);
+}
