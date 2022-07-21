9de9a1ce32cad13d04a385442a4ff47b9ef464c9
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2022-07-21 21:26:39 +0300

    BL finished

---
+++ SimpleTextEditor/BusinessLogic/Editor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Editor //Originator
+    {
+        private string _content="";
+
+        public string Content { get { return _content; } set { _content = value; } }
+
+        public Editor()
+        {
+            Content = "";
+        }
+
+        public EditorState Save()
+        {
+            return new EditorState(_content);
+        }
+        public void Load(EditorState state)
+        {
+            Content = state.Content;
+        }
+    }
+}
---
+++ SimpleTextEditor/BusinessLogic/EditorState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class EditorState //Memento
+    {
+        private readonly string content;
+        public string Content { get { return content; } }
+
+        public EditorState(string content)
+        {
+            this.content = content;
+        }
+    }
+}
