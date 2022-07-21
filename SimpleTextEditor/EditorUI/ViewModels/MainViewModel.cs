1b7a65ec9d7351328d120bde3d122d9739e9f8d4
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2022-07-21 19:38:53 +0300

    Transfered to MVVM partern

---
+++ SimpleTextEditor/EditorUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorUI.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            _history = new History();
+            _editor = new Editor();
+        }
+
+        public History _history { get; set; }
+        public Editor _editor { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
