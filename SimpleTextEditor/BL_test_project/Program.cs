d3de1303ea93e0d0538b2f8e376473c00b03c727
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2022-07-21 19:47:57 +0300

    First Test of BL

---
+++ SimpleTextEditor/BL_test_project/Program.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLogic;
+
+Editor _editor = new Editor();
+History _history = new History();
+
+_editor.Content = "Hello World!";
+_history.Save(_editor);
+_editor.Content = "Hello World! 2";
+_history.Save(_editor);
+_editor.Content = "Hello World! 3";
+_history.Save(_editor);
+Console.WriteLine("Content: " + _editor.Content);
+Console.WriteLine("History: " + _history.CanUndo);
+Console.WriteLine("History: " + _history.CanRedo);
+//_editor.Load(_history.UndoAction());
